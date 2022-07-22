b12944428a2cc50e48a2dae7c9b0d6d8d9b03271
Lucas <lucassanchezdl@gmail.com>
2022-07-22 15:36:00 -0300

    join

---
+++ join.js
@@ -0,0 +1,7 @@
+//------- Método join --------
+/*Une todos los elementos de un array en una cadena. Podemos pasarle como parámetro el caracter de separación que debe agregar entre cada elemento*/
+
+const heroes = ["Batman", "Flash", "Thor", "Ironman", "Hulk"];
+
+const cadenaDeHeroes = heroes.join("-");
+console.log(cadenaDeHeroes);

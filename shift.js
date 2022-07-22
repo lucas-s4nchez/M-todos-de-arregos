a9dbfbdd14cc32b7f9e9c47f150e151880f2867c
Lucas <lucassanchezdl@gmail.com>
2022-07-22 17:27:07 -0300

    shift & unshift

---
+++ shift.js
@@ -0,0 +1,14 @@
+//------- Método shift --------
+/*Elimina el primer elemento del array.*/
+
+const carrito = [
+  { nombre: "Monitor 27 Pulgadas", precio: 500 },
+  { nombre: "Televisión", precio: 100 },
+  { nombre: "Tablet", precio: 300 },
+  { nombre: "Audifonos", precio: 200 },
+  { nombre: "Teclado", precio: 400 },
+  { nombre: "Celular", precio: 700 },
+];
+
+carrito.shift();
+console.log(carrito);

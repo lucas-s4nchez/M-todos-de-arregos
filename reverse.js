0ba795d6bafcc33d65be827001b5dd5c04021d15
Lucas <lucassanchezdl@gmail.com>
2022-07-22 18:20:39 -0300

    reverse

---
+++ reverse.js
@@ -0,0 +1,8 @@
+//------- Método splice--------
+/*Cambia el orden de los elementosde un array. El priumer elemento pasa a ser el último y el último pasa a ser el primero*/
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log("Orden original: ", numeros);
+
+console.log("Orden revertido: ", numeros.reverse());
